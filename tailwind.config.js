3fda38b6a3b6ba775dec688fd3faa962449b5f75
Fawwaz129 <wazwazzsrhrr@gmail.com>
2022-11-18 08:24:58 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: '20px',
+    },
+    fontFamily: {
+      'inter': ['Inter', 'sans-serif'],
+      'opensans': ['Open Sans', 'cursive'],
+      'Montserrat': ['Montserrat', 'sans-serif'],
+      'ComicNeue': ['Comic Neue', 'sans-serif'],
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("daisyui"),
+  ],
 }
